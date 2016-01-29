e204a4033e8d00c5ea3e0a3bb292493a3fe33bae
Matteo Traina <matteo.traina@rightmove.co.uk>
2016-01-29 16:37:52 +0000

    endpoint changes

--- com/mtraina/go.rest.api/service.go
+++ com/mtraina/go.rest.api/service.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"encoding/json"
 )
 
+type Todo struct {
+	Name 		string	`json:"name"`
+	Completed 	bool	`json:"completed"`
+}
+
+type Todos []Todo
+
 func main() {
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos {
+		Todo { Name: "Foo", Completed: false },
+		Todo { Name: "Bar", Completed: true  },
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
