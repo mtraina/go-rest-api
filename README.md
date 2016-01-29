2eb54dca9e586947762eefbf75eca8bbf897fe5f
Matteo Traina <matteo.traina@rightmove.co.uk>
2016-01-29 16:04:06 +0000

    gorilla framework added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # go-rest-api
+
+Implementation following the following tutorial
+https://thenewstack.io/make-a-restful-json-api-go/
--- com/mtraina/go.rest.api/service.go
+++ com/mtraina/go.rest.api/service.go
@@ -5,13 +5,16 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
