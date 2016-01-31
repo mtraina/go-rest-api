0144816844344f9f59927a4d7ae9020e132361f3
mtraina <matteo.traina.mail@gmail.com>
2016-01-31 22:30:09 +0100

    set status code and content type in the header

--- com/mtraina/go.rest.api/handlers.go
+++ com/mtraina/go.rest.api/handlers.go
@@ -17,7 +17,12 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo { Name: "Bar", Completed: true },
 	}
 
-	json.NewEncoder(w).Encode(todos)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
