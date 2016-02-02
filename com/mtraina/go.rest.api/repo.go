6c08152c44177489c3898fa368065c4baf755059
mtraina <matteo.traina.mail@gmail.com>
2016-02-02 15:02:00 +0100

    added search and delete to repo

--- com/mtraina/go.rest.api/repo.go
+++ com/mtraina/go.rest.api/repo.go
@@ -1,4 +1,5 @@
 package main
+import "fmt"
 
 var currentId int
 var todos Todos
@@ -8,9 +9,28 @@ func init()  {
 	RepoCreateTodo(Todo{Name: "Host meetup"})
 }
 
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.Id == id {
+			return t
+		}
+	}
+	return Todo{}
+}
+
 func RepoCreateTodo(t Todo) Todo {
 	currentId += 1
 	t.Id = currentId
 	todos = append(todos, t)
 	return t
+}
+
+func RepoDestroyTodo(id int) error {
+	for i, t := range todos {
+		if t.Id == id {
+			todos = append(todos[:i], todos[i + 1:] ...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
 }
