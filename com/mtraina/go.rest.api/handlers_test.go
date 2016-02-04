e1c2726d996b1c1640e610978bc5224122bb7718
mtraina <matteo.traina.mail@gmail.com>
2016-02-04 12:02:21 +0100

    ginkgo and gomega testing framework added

--- com/mtraina/go.rest.api/handlers_test.go
+++ com/mtraina/go.rest.api/handlers_test.go
@@ -1,34 +1,69 @@
 package main
 
 import (
-	"testing"
-	"net/http/httptest"
 	"net/http"
 	"fmt"
 	"encoding/json"
+
+	"testing"
+	"net/http/httptest"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestTodoIndex(t *testing.T){
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "/", nil)
+var _ = Describe("Handlers", func() {
 
-	if err != nil {
-		t.Fatal("request creation error")
-	}
+	var (
+		w *httptest.ResponseRecorder
+	)
 
-	TodoIndex(w, r)
+	BeforeEach(func() {
+		w = httptest.NewRecorder()
+	})
 
-	if w.Code != 200 {
-		t.Fatalf("wrong status code: {}", w.Code)
-	}
+	Describe("Get todos", func() {
+		Context("Plain", func() {
+			It("should return 2 todos", func() {
+				r, err := http.NewRequest("GET", "/", nil)
+				Expect(err).NotTo(HaveOccurred())
 
-	var ts Todos
-	err = json.NewDecoder(w.Body).Decode(&ts)
+				TodoIndex(w, r)
 
+				var ts Todos
+				err = json.NewDecoder(w.Body).Decode(&ts)
 
+				fmt.Printf("the returned body is {}", ts)
+			})
+		})
+	})
+})
 
-	fmt.Printf("the returned body is {}", ts)
+func TestHandlers(t *testing.T){
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Handler suite")
 }
+
+//func TestTodoIndex(t *testing.T){
+//	w := httptest.NewRecorder()
+//	r, err := http.NewRequest("GET", "/", nil)
+//
+//	if err != nil {
+//		t.Fatal("request creation error")
+//	}
+//
+//	TodoIndex(w, r)
+//
+//	if w.Code != 200 {
+//		t.Fatalf("wrong status code: {}", w.Code)
+//	}
+//
+//	var ts Todos
+//	err = json.NewDecoder(w.Body).Decode(&ts)
+//
+//
+//
+//	fmt.Printf("the returned body is {}", ts)
+//}
 
 //func TestCreateTodo(t *testing.T){
 //	TodoCreate()
