f1bc46182a20bfe16c22858a5fcad1570b077071
mtraina <matteo.traina.mail@gmail.com>
2016-02-19 16:50:32 +0100

    routes small change

--- com/mtraina/go.rest.api/routes.go
+++ com/mtraina/go.rest.api/routes.go
@@ -15,5 +15,5 @@ var routes = Routes {
 	Route { "Index", "GET", "/", Index },
 	Route { "TodoIndex", "GET", "/todos", TodoIndex },
 	Route { "TodoShow", "GET", "/todos/{todoId}", TodoShow },
-	Route{ "TodoCreate", "POST", "/todos", TodoCreate },
+	Route { "TodoCreate", "POST", "/todos", TodoCreate },
 }
