5bf01acfe5a0f4c97750f20c0792c962ce9d146d
mtraina <matteo.traina.mail@gmail.com>
2016-02-19 16:51:19 +0100

    messing up with the old repo and adding bolt support on it

--- com/mtraina/go.rest.api/repo.go
+++ com/mtraina/go.rest.api/repo.go
@@ -1,22 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"time"
+)
 
 var currentId int
 var todos Todos
+var db *bolt.DB
 
 func init()  {
-  	RepoCreateTodo(Todo{Name: "Write presentation"})
-	RepoCreateTodo(Todo{Name: "Host meetup"})
+	fmt.Printf("start!!")
+
+	db2, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	// todo: crap!
+	db = db2
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//err := db.Update(func(tx *bolt.Tx) error {
+	err2 := db.Update(func(tx *bolt.Tx) error {
+		//return nil
+		b, err := tx.CreateBucket([]byte("Todos"))
+		if err != nil {
+			return err
+		}
+
+		return b.Put([]byte("todo"), []byte("Write presentation2"))
+	})
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+  	//RepoCreateTodo(Todo{Name: "Write presentation"})
+	//RepoCreateTodo(Todo{Name: "Host meetup"})
 }
 
-func RepoFindTodo(id int) Todo {
-	for _, t := range todos {
-		if t.Id == id {
-			return t
-		}
-	}
-	return Todo{}
+func RepoFindTodo(id int) (string, error) {
+	var value string
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Todos"))
+		v := b.Get([]byte("todo"))
+		value = string(v)
+		return nil
+	})
+
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+
+	return value, err
+
+
+//	for _, t := range todos {
+//		if t.Id == id {
+//			return t
+//		}
+//	}
+//	return Todo{}
 }
 
 func RepoCreateTodo(t Todo) Todo {
